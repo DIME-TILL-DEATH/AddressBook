3dfad9fd0ddfc46235f15392347a55d25998bd8e
Dmitriy Kostiuchik <uaas@list.ru>
2021-06-03 16:54:47 +0300

    New project structure

---
+++ DBManager/DBManager.pro
@@ -0,0 +1,9 @@
+QT -= gui
+QT += core sql
+CONFIG += C++11
+TEMPLATE = lib
+
+DESTDIR = $$PWD/../shared/lib
+
+HEADERS += \
+    include/dbtypes.h
---
+++ DBManager/include/dbtypes.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace db
+{
+    enum class DBResult{
+        OK,
+        FAIL
+    };
+
+    enum class DBTables{
+        Contacts
+    };
+}
